3dc81571a442cb6be9ee45cce372e44dfb2eea6c
yoramLen <66311731+yoramLen@users.noreply.github.com>
2021-01-15 13:16:29 +0100

    git bash wou niet werken, vorige repo stond op bitbucket
    ffouten in de vorige uitwerking, opnieuw begonnen.

---
+++ gatsby-config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  siteMetadata: {
+    title: `Lenaers Agency`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+
+      resolve: "gatsby-source-graphql",
+      
+      options: {
+      
+      typeName: "WPGraphql",
+      
+      fieldName: "wpcontent",
+      
+      url: "http://lenaers-agency.local",
+      
+      },
+      
+      },
+      {
+
+        resolve: `gatsby-plugin-google-fonts`,
+        
+        options: {
+        
+        fonts: [`Roboto`, `Oswald`],
+        
+        display: "swap",
+        
+        },
+        
+        }, 
+        `gatsby-plugin-styled-components`
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
